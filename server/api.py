15285d5cb2bf4875fe7317a6ba5eae4415b135e9
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-01-28 12:03:15 +0100

    Make the jira integration more testable (#15)
    * Make the jira integration more testable

--- server/api.py
+++ server/api.py
@@ -1,16 +1,22 @@
-import json
 import logging
 from subprocess import CalledProcessError
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
+from requests import HTTPError
 
+from .clients import JiraClient
 from .create_jira_issue import ProjectDetails, create_issue_basic
 
 app = FastAPI()
 instrumentator = Instrumentator(excluded_handlers=["/health/.*", "/metrics"])
 instrumentator.instrument(app).expose(app)
+
+
+def get_jira_client():
+    return JiraClient("https://statistics-norway.atlassian.net/rest/api/3")
+
 
 origins = ["*"]
 
@@ -44,15 +50,20 @@ def health_readiness():
 
 
 @app.post("/create_jira", status_code=201)
-def create_issue(details: ProjectDetails):
+def create_issue(details: ProjectDetails, client: JiraClient = Depends(get_jira_client)):
     """
     Endpoint for Jira issue creation using basic auth
     """
     try:
         logging.info(f"Got a jira issue creation request. Details:\n{details.json()}")
-        response_from_jira = create_issue_basic(details)
-        logging.debug(response_from_jira)
-        return json.loads(response_from_jira.text)
-    except (CalledProcessError, Exception) as error:
+        return client.create_issue(create_issue_basic(details))
+    except CalledProcessError as error:
+        logging.exception("Error occurred: %s", error)
+        raise HTTPException(status_code=500, detail=f"Error occurred:\n\n{error.stdout.decode()}")
+    except HTTPError as error:
+        logging.exception("Error occurred: %s", error)
+        raise HTTPException(status_code=500, detail=f"Error occurred:\n\n{error.response.text}")
+    except Exception as error:
         logging.exception("Error occurred: %s", error)
         raise HTTPException(status_code=500, detail=f"Error occurred:\n\n{error}")
+
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,10 @@
 from fastapi.testclient import TestClient
-from server.api import app
+
+from server.clients import JiraClient
+from server.api import app, get_jira_client
+
 client = TestClient(app)
+jira_client_mock = JiraClient("dummy-path")
 
 
 def test_health_liveness():
@@ -19,3 +23,21 @@ def test_health_readiness():
         "name": "dapla-start-api",
         "status": "UP"
     }
+
+
+def test_create_issue():
+    # https://fastapi.tiangolo.com/advanced/testing-dependencies/?h=override
+    app.dependency_overrides[get_jira_client] = lambda: jira_client_mock
+    jira_response = {
+        "id": "112",
+        "key": "DS-1",
+        "self": "https://statistics-norway.atlassian.net/rest/api/3/issue/112"
+    }
+    jira_client_mock.create_issue = lambda arg: jira_response
+
+    response = client.post("/create_jira", json={
+        "display_team_name": "My team",
+        "manager": "mymanager"
+    })
+    assert response.status_code == 201
+    assert response.json() == jira_response
