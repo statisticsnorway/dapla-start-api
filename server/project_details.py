ed8226fd2b4c1f1732f130448240634d170816bd
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-02-01 08:23:01 +0100

    Extract Reporter from JWT-token. Model changes to ProjectDetails. (#17)
    * Extract Reporter from JWT-token. Model changes to ProjectDetails.

--- server/project_details.py
+++ server/project_details.py
@@ -3,11 +3,20 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
+class ProjectUser(BaseModel):
+    name: str
+    email: str
+
+
 class ProjectDetails(BaseModel):
     display_team_name: str
-    manager: str
-    data_protection_officers: Optional[List[str]]
-    developers: Optional[List[str]]
-    consumers: Optional[List[str]]
+    manager: ProjectUser
+    data_protection_officers: Optional[List[ProjectUser]]
+    developers: Optional[List[ProjectUser]]
+    consumers: Optional[List[ProjectUser]]
     enabled_services: Optional[List[str]]
     authorization_code: Optional[str]
+    reporter: Optional[ProjectUser]
+    other_info: Optional[str]
+    ui_version: Optional[str]
+    api_version: Optional[str]
