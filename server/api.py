6b7ba3435b5885aa7366b97ba708b47096dadc7c
Kenneth Leine Schulstad <ks@udp.no>
2022-02-01 15:51:53 +0100

    Add list users endpoint

--- server/api.py
+++ server/api.py
@@ -1,4 +1,6 @@
 import logging
+import csv
+import os
 import jwt
 from server import __version__
 from subprocess import CalledProcessError
@@ -12,6 +14,8 @@ from typing import Optional
 from .clients import JiraClient
 from .project_details import ProjectDetails, ProjectUser
 from .create_jira_issue import create_issue_basic
+
+SSB_USERS_SOURCE = os.environ.get("SSB_USERS_SOURCE", "tests/test-users-export.csv")
 
 app = FastAPI()
 instrumentator = Instrumentator(excluded_handlers=["/health/.*", "/metrics"])
@@ -83,3 +87,56 @@ def create_issue(details: ProjectDetails, authorization: Optional[str] = Header(
         logging.exception("Error occurred: %s", error)
         raise HTTPException(status_code=500, detail=f"Error occurred:\n\n{error}")
 
+
+@app.get("/users")
+def list_users(fields: str = None):
+    """List all applicable SSB users.
+
+    Only return users that:
+    * Has a valid first- and surname
+    * Has an active account
+    * Has a short email starting with 3 letters OR has a "kons" prefix
+    """
+    all_fields = set(["name", "email", "email_short"])
+    fields_filter = set([f.strip() for f in fields.split(',')]) if fields else all_fields
+    fields_filter = fields_filter.intersection(all_fields)
+    if len(fields_filter) == 0:
+        fields_filter = all_fields
+
+    users = []
+    with open(SSB_USERS_SOURCE, mode='r', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+
+            skip = False
+            display_name = row["displayName"].strip()
+            principal = row['userPrincipalName'].strip()
+
+            # Skip users without proper name
+            if len(display_name) == 0:
+                skip = True
+
+            # Only keep accounts with principals with "3-letter" and those starting with "kons"
+            elif not (len(principal) > 4 and (principal[3] == '@' or principal.startswith('kons'))):
+                skip = True
+
+            if not skip:
+                user = {}
+
+                if 'name' in fields_filter:
+                    user['name'] = display_name.replace("  ", ", ", 1)
+
+                if 'email' in fields_filter:
+                    user['email'] = str(row['mail']).lower()
+
+                if 'email_short' in fields_filter:
+                    user['email_short'] = principal
+
+                users.append(user)
+
+            line_count += 1
+
+    return users
