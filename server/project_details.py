2efd41bed447a0241b389085fdae506f98fc0c76
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-02-03 10:19:23 +0100

    Bugfix for create_jira (#21)
    * Fixed fragile test, validate ADF against schema in test and fixed missing email_short for reported by user.
    * Extracted jwt-parsing to separate (testable) method.

--- server/project_details.py
+++ server/project_details.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
-
 from pydantic import BaseModel
+import jwt
 
 
 class ProjectUser(BaseModel):
@@ -21,3 +21,13 @@ class ProjectDetails(BaseModel):
     other_info: Optional[str]
     ui_version: Optional[str]
     api_version: Optional[str]
+
+
+def project_user_from_jwt(token: str):
+    decoded = jwt.decode(token, options={
+        "verify_signature": False,
+        "verify_aud": False,
+        "verify_exp": False
+    }, algorithms=["HS256", "RS256"])
+    email_short = decoded['preferred_username'] if 'preferred_username' in decoded else decoded['email']
+    return ProjectUser(name=decoded['name'], email=decoded['email'], email_short=email_short)
