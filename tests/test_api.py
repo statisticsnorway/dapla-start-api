afdd57ca1620e2a65acefb3d1e7f5708e62ee142
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2022-02-07 09:08:56 +0100

    Added tool for consistent versioning

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,10 @@
+import toml
 from fastapi.testclient import TestClient
 
 from server.clients import JiraClient
 from server.api import app, get_jira_client
+from pathlib import Path
+from server import __version__
 
 client = TestClient(app)
 jira_client_mock = JiraClient("dummy-path")
@@ -23,6 +26,18 @@ def test_health_readiness():
         "name": "dapla-start-api",
         "status": "UP"
     }
+
+
+def test_versions_are_in_sync():
+    """Checks if the pyproject.toml and package.__init__.py __version__ are in sync."""
+
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
+
+    package_init_version = __version__
+
+    assert package_init_version == pyproject_version
 
 
 def test_create_issue():
