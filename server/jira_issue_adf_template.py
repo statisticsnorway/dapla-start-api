6267ce8d18681fe2294ecd17f875f22537d8b4c9
Miles Winther <42948872+mmwinther@users.noreply.github.com>
2022-09-13 15:12:59 +0200

    Tilsette -> legge til

--- server/jira_issue_adf_template.py
+++ server/jira_issue_adf_template.py
@@ -665,7 +665,7 @@ def _description(details: ProjectDetails, current_date: date = None):
                                                 "content": [
                                                     {
                                                         "type": "text",
-                                                        "text": "Tilsette repo URL på slutten av "
+                                                        "text": "Legge til repo URL på slutten av "
                                                     },
                                                     {
                                                         "type": "text",
