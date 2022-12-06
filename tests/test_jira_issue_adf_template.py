7a40d9453482bc2f36131715aff1a0b6f36a992a
Miles Winther <42948872+mmwinther@users.noreply.github.com>
2022-12-06 13:19:03 +0100

    Factor access_groups table out of Jira template
    - Remove localId to prevent potential collision
    - Formatting with Black
    - Use fixtures in tests
    - Improve granularity of tests to make debugging possible

--- tests/test_jira_issue_adf_template.py
+++ tests/test_jira_issue_adf_template.py
@@ -2,28 +2,68 @@ from tests import resolve_filename
 from server.jira_issue_adf_template import _description
 from server.project_details import ProjectDetails, ProjectUser, OrganizationInfo
 import json
+import pytest
 from jsonschema import validate
 import requests
 from datetime import date
 
 
-def test_create_issue():
-    project_details = ProjectDetails(
+@pytest.fixture()
+def project_details():
+    return ProjectDetails(
         display_team_name="Team Stubbe",
         uniform_team_name="stubbs",
-        manager=ProjectUser(name="Magnus Manager", email_short="mm@ssb.no", email="magnus.manager@ssb.no"),
-        data_admins=[ProjectUser(name="Pernille Pilot", email_short="ppi@ssb.no", email="ppi@ssb.no"),
-                                  ProjectUser(name="Petter Andrepilot ", email_short="pap@ssb.no", email="pap@ssb.no")],
-        developers=[ProjectUser(name="Dorte Developer", email_short="dd@ssb.no", email="dd@ssb.no"),
-                    ProjectUser(name="Diana Developer", email_short="did@ssb.no", email="did@ssb.no")],
+        manager=ProjectUser(
+            name="Magnus Manager",
+            email_short="mm@ssb.no",
+            email="magnus.manager@ssb.no",
+        ),
+        data_admins=[
+            ProjectUser(
+                name="Pernille Pilot", email_short="ppi@ssb.no", email="ppi@ssb.no"
+            ),
+            ProjectUser(
+                name="Petter Andrepilot ", email_short="pap@ssb.no", email="pap@ssb.no"
+            ),
+        ],
+        developers=[
+            ProjectUser(
+                name="Dorte Developer", email_short="dd@ssb.no", email="dd@ssb.no"
+            ),
+            ProjectUser(
+                name="Diana Developer", email_short="did@ssb.no", email="did@ssb.no"
+            ),
+        ],
         org_info=OrganizationInfo(code="11", name="Seksjon 11", parent_code="1"),
-        reporter=ProjectUser(name="Reidar Reporter", email_short="rr@ssb.no", email="rr@ssb.no"),
-        other_info="Some other info"
+        reporter=ProjectUser(
+            name="Reidar Reporter", email_short="rr@ssb.no", email="rr@ssb.no"
+        ),
+        other_info="Some other info",
     )
+
+@pytest.fixture()
+def description(project_details):
+    return _description(project_details, date.fromisoformat("2022-02-01"))
+
+@pytest.fixture()
+def json_schema():
+    return requests.get(
+        "https://unpkg.com/@atlaskit/adf-schema@latest/dist/json-schema/v1/full.json"
+    ).json()
+
+
+def test_create_issue_valid_json_schema(description, json_schema):
     # Validate against Atlassian Document Format schema
-    jsonschema = requests.get("https://unpkg.com/@atlaskit/adf-schema@latest/dist/json-schema/v1/full.json").json()
+    validate(instance=description, schema=json_schema)
+
+def test_create_issue_generated_dict(description):
     with open(resolve_filename("adf_template_result.json"), encoding="utf-8") as file:
-        description = _description(project_details, date.fromisoformat('2022-02-01'))
-        validate(instance=description, schema=jsonschema)
+        expected = json.load(file)
+    assert len(description["content"]) == len(expected["content"])
+    for actual, expected in zip(description["content"], expected["content"]):
+        assert actual == expected
+
+def test_create_issue_exact_file_text(description):
+    with open(resolve_filename("adf_template_result.json"), encoding="utf-8") as file:
         result = json.dumps(description, indent=2, ensure_ascii=False) + "\n"
         assert result == file.read()
