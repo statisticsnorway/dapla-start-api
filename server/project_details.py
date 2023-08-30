0156d69059ac8290e9ebca57890bc09f87b9615b
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2023-08-30 16:53:44 +0200

    :arrow_up: Update dependencies (#64)
    * :arrow_up: Update dependencies
    
    Signed-off-by: mmwinther <noreply@github.com>
    
    * Handle Pydantic v2 changes
    
    * Add snykOrganization parameter to build template
    
    * Update Snyk auth in the pipeline
    
    ---------
    
    Signed-off-by: mmwinther <noreply@github.com>
    Co-authored-by: mmwinther <noreply@github.com>

--- server/project_details.py
+++ server/project_details.py
@@ -17,26 +17,32 @@ class OrganizationInfo(BaseModel):
 
 class ProjectDetails(BaseModel):
     display_team_name: str
-    uniform_team_name: Optional[str]
+    uniform_team_name: Optional[str] = None
     manager: ProjectUser
-    data_admins: Optional[List[ProjectUser]]
-    developers: Optional[List[ProjectUser]]
-    consumers: Optional[List[ProjectUser]]
-    support: Optional[List[ProjectUser]]
-    org_info: Optional[OrganizationInfo]
-    enabled_services: Optional[List[str]]
-    authorization_code: Optional[str]
-    reporter: Optional[ProjectUser]
-    other_info: Optional[str]
-    ui_version: Optional[str]
-    api_version: Optional[str]
+    data_admins: Optional[List[ProjectUser]] = None
+    developers: Optional[List[ProjectUser]] = None
+    consumers: Optional[List[ProjectUser]] = None
+    support: Optional[List[ProjectUser]] = None
+    org_info: Optional[OrganizationInfo] = None
+    enabled_services: Optional[List[str]] = None
+    authorization_code: Optional[str] = None
+    reporter: Optional[ProjectUser] = None
+    other_info: Optional[str] = None
+    ui_version: Optional[str] = None
+    api_version: Optional[str] = None
 
 
 def project_user_from_jwt(token: str):
-    decoded = jwt.decode(token, options={
-        "verify_signature": False,
-        "verify_aud": False,
-        "verify_exp": False
-    }, algorithms=["HS256", "RS256"])
-    email_short = decoded['preferred_username'] if 'preferred_username' in decoded else decoded['email']
-    return ProjectUser(name=decoded['name'], email=decoded['email'], email_short=email_short)
+    decoded = jwt.decode(
+        token,
+        options={"verify_signature": False, "verify_aud": False, "verify_exp": False},
+        algorithms=["HS256", "RS256"],
+    )
+    email_short = (
+        decoded["preferred_username"]
+        if "preferred_username" in decoded
+        else decoded["email"]
+    )
+    return ProjectUser(
+        name=decoded["name"], email=decoded["email"], email_short=email_short
+    )
