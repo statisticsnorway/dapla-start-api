2343a8bd57e147a523c5824c62bdea5f4750aa19
mmwinther <mmw@ssb.no>
2023-09-07 14:03:46 +0200

    Combine stdout and stderr

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -5,17 +5,18 @@ def test_server_starts():
     try:
         server = subprocess.run(
             ["uvicorn", "server.api:app", "--host", "0.0.0.0"],
-            capture_output=True,
             timeout=2,
             check=True,
             text=True,
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
         )
     except subprocess.TimeoutExpired as e:
         # This is expected/desired behaviour
-        assert "Application startup complete." in str(e.stderr)
+        assert "Application startup complete." in str(e.stdout)
     except subprocess.CalledProcessError as e:
         print("Process output:")
-        print("".join(e.stderr))
+        print("".join(e.stdout))
         assert e.returncode == 0
     else:
-        assert "Application startup complete." in str(server.stderr)
+        assert "Application startup complete." in str(server.stdout)
